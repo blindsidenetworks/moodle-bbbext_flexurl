52c5e5dcb6bd0df2fd06a4f14a054a5e9090471a
Shamiso.Jaravaza <33659194+ssj365@users.noreply.github.com>
2023-12-14 08:28:34 -0700

    CONTRIB-9449: Update tests
    * Add check on create URL and join URLs for extra parameters

--- tests/bigbluebutton/action_url_addons_tests.php
+++ tests/bigbluebutton/action_url_addons_tests.php
@@ -18,6 +18,7 @@ namespace bbbext_flexurl\bigbluebuttonbn;
 use bbbext_flexurl\utils;
 use mod_bigbluebuttonbn\extension;
 use mod_bigbluebuttonbn\external\get_join_url;
+use mod_bigbluebuttonbn\local\proxy\proxy_base;
 use mod_bigbluebuttonbn\instance;
 
 /**
@@ -65,33 +66,75 @@ class action_url_addons_tests extends \advanced_testcase {
         $DB->insert_record('bbbext_flexurl',
             ['bigbluebuttonbnid' => $this->bbb->id, 'eventtype' => utils::ACTION_CODES['all'], 'paramname' => 'coursename',
                 'paramvalue' => 'courseinfo.fullname', ]);
+        // Pass extra parameters that are prefixed, meta or contain dashes on both create and join.
+        $DB->insert_record('bbbext_flexurl',
+            ['bigbluebuttonbnid' => $this->bbb->id, 'eventtype' => utils::ACTION_CODES['all'], 'paramname' => 'one',
+            'paramvalue' => 'TEST', ]);
+        $DB->insert_record('bbbext_flexurl',
+            ['bigbluebuttonbnid' => $this->bbb->id, 'eventtype' => utils::ACTION_CODES['all'], 'paramname' => 'suffix_two',
+            'paramvalue' => 'TEST', ]);
+        $DB->insert_record('bbbext_flexurl',
+            ['bigbluebuttonbnid' => $this->bbb->id, 'eventtype' => utils::ACTION_CODES['all'], 'paramname' => 'meta_three',
+            'paramvalue' => 'TEST', ]);
+        $DB->insert_record('bbbext_flexurl',
+            ['bigbluebuttonbnid' => $this->bbb->id, 'eventtype' => utils::ACTION_CODES['all'], 'paramname' => 'four-param',
+            'paramvalue' => 'TEST', ]);
     }
 
     /**
      * Test join URL
      *
+     * @dataProvider provider_value_for_parameters
+     *
+     * @param string $expectedstring
      * @return void
      */
-    public function test_join_url_has_options() {
+    public function test_join_url_has_options($expectedstring) {
         $this->setUser($this->user);
         $instance = instance::get_from_instanceid($this->bbb->id);
         $joinurl = get_join_url::execute($instance->get_cm_id());
+        $this->assertStringNotContainsString('firstname', $joinurl['join_url']);
         $this->assertStringContainsString('lastname', $joinurl['join_url']);
         $this->assertStringContainsString('coursename', $joinurl['join_url']);
         $this->assertStringContainsString('BBB+LN', $joinurl['join_url']);
+        $this->assertStringContainsString($expectedstring, $joinurl['join_url']);
     }
 
     /**
      * Test create URL
      *
+     * @dataProvider provider_value_for_parameters
+     *
+     * @param string $expectedstring
      * @return void
      */
-    public function test_create_url_has_options() {
+    public function test_create_url_has_options($expectedstring) {
         $this->setUser($this->user);
         instance::get_from_instanceid($this->bbb->id);
         $addons = extension::action_url_addons('create', [], ['bbb-meta' => 'Test'], $this->bbb->id);
         $this->assertContains('firstname', array_keys($addons['metadata']));
         $this->assertContains('coursename', array_keys($addons['metadata']));
         $this->assertNotContains('lastname', array_keys($addons['metadata']));
+
+        $rc = new \ReflectionClass(proxy_base::class);
+        $actionurl = $rc->getMethod('action_url');
+        $actionurl->setAccessible(true);
+        // Generate a create URL.
+        $createurl = $actionurl->invokeArgs(null, ['create', [], [], $this->bbb->id]);
+        $this->assertStringContainsString($expectedstring, $createurl);
+    }
+
+    /**
+     * Provider for Test create URL and join URL
+     *
+     * @return array[]
+     */
+    public static function provider_value_for_parameters(): array {
+        return [
+            'pass a parameter on create and join' => ['one=TEST'],
+            'pass a prefixed parameter on create and join' => ['suffix_two=TEST'],
+            'pass a meta parameter on create and join' => ['meta_three=TEST'],
+            'pass a parameter with dashes on create and join' => ['four-param=TEST'],
+        ];
     }
 }
